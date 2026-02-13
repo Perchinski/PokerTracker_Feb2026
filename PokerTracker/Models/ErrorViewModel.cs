c188431e965b297eb922516011dad7c018b08bc0
Perchinski <aleksperchinski@gmail.com>
2026-02-14 00:14:33 +0200

    Add project files.

---
+++ PokerTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PokerTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
